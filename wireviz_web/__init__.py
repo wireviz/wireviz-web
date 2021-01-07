1e41aaa8561e8f0041a650b4d7f42c716a0e756c
Andreas Motl <andreas.motl@panodata.org>
2021-01-07 18:41:49 +0100

    Adjust coverage testing

--- wireviz_web/__init__.py
+++ wireviz_web/__init__.py
@@ -5,13 +5,13 @@ __appname__ = "wireviz-web"
 # Single-sourcing the package version
 # https://cjolowicz.github.io/posts/hypermodern-python-06-ci-cd/
 try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
+    from importlib.metadata import PackageNotFoundError, version  # noqa
+except ImportError:  # pragma: no cover
     from importlib_metadata import PackageNotFoundError, version  # noqa
 
 try:
     __version__ = version(__name__)
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 
 
